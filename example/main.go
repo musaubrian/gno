5a165fea8f61a10f4d120a99a8b53ccc71e64ca9
musaubrian <musaubrian15@gmail.com>
2024-01-13 17:10:40 +0300

    chore: add mit license
    - export log

--- example/main.go
+++ example/main.go
@@ -5,21 +5,24 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	g "github.com/musaubrian/gno"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!\n")
-	log.Printf("endpoint '%s' hit", r.RequestURI)
+	g.Log(g.INFO, fmt.Sprintf("endpoint '%s' hit", r.RequestURI))
 }
+
 func now(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s\n", time.Now().Format(time.RFC822))
-	log.Printf("endpoint '%s' hit", r.RequestURI)
+	g.Log(g.INFO, fmt.Sprintf("endpoint '%s' hit", r.RequestURI))
 }
 
 func main() {
 	port := ":8080"
-	http.HandleFunc("/", greet)
+	http.HandleFunc("/", home)
 	http.HandleFunc("/now", now)
-	log.Println("Server running at: ", port)
+	g.Log(g.INFO, "Server running at: "+port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
